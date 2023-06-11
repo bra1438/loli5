14756c7ba806765ad03eb48c6de1a73d4569d90b
Brahim <32096282+bra1438@users.noreply.github.com>
2023-06-11 14:47:36 +0300

    Update app.py

--- app.py
+++ app.py
@@ -19,11 +19,12 @@ vowels = ["ا", "و", "ي"]
 def pronounce_arabic_letter_with_vowel(letter, vowel):
     for vowel in vowels:
         tts = gTTS(text=letter + vowel, lang='ar')
-        tts.save(f"file_{vowel}.mp3" )
-        st.audio("output.mp3")
+        tts.save(f"file_{vowel}.mp3")
+        st.audio(f"file_{vowel}.mp3")
 
-st.title("تعليم نطق الخروف العربية ")
+st.title("Arabic Letter Pronunciation")
 
-letter = st.text_input("ادخل الحرف:")
+letter = st.text_input("Enter an Arabic letter:")
+
 if letter:
-    pronounce_arabic_letter_with_vowel(letter, vowel)
+    pronounce_arabic_letter_with_vowel(letter)
