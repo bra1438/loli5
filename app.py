8bcdddabc6d54ff0bda47ba2ea8ac2796b69cb30
Brahim <32096282+bra1438@users.noreply.github.com>
2023-06-11 15:15:16 +0300

    Update app.py

--- app.py
+++ app.py
@@ -16,3 +16,13 @@ if letter:
     for vowel in vowels:
         pronounce_arabic_letter_with_vowel(letter, vowel)
 
+        
+# Create a list of Arabic letters
+arabic_letters = ["ا", "ب", "ت", "ث", "ج", "ح", "خ", "د", "ذ", "ر", "ز", "س", "ش", "ص", "ض", "ط", "ظ", "ع", "غ", "ف", "ق", "ك", "ل", "م", "ن", "ه", "و", "ي"]
+
+# Create a dropdown list with the Arabic letters
+letter_dropdown = st.selectbox("Select an Arabic letter", arabic_letters)
+
+# Print the selected Arabic letter
+if letter_dropdown:
+    st.write(f"You selected the Arabic letter: {letter_dropdown}")
