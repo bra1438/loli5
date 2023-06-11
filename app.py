803b72792f72ba427625182804970f7df3ad61a8
Brahim <32096282+bra1438@users.noreply.github.com>
2023-06-11 15:20:50 +0300

    Update app.py

--- app.py
+++ app.py
@@ -10,20 +10,13 @@ def pronounce_arabic_letter_with_vowel(letter, vowel):
 
 st.title("Arabic Letter Pronunciation")
 
-letter = st.text_input("Enter an Arabic letter:")
-
-if letter:
-    for vowel in vowels:
-        pronounce_arabic_letter_with_vowel(letter_dropdown, vowel)
-
-        
 # Create a list of Arabic letters
 arabic_letters = ["ا", "ب", "ت", "ث", "ج", "ح", "خ", "د", "ذ", "ر", "ز", "س", "ش", "ص", "ض", "ط", "ظ", "ع", "غ", "ف", "ق", "ك", "ل", "م", "ن", "ه", "و", "ي"]
 
 # Create a dropdown list with the Arabic letters
 letter_dropdown = st.selectbox("Select an Arabic letter", arabic_letters)
 
-# Print the selected Arabic letter
+# If a letter is selected, pronounce it with all three vowels
 if letter_dropdown:
-    st.write(f"You selected the Arabic letter: {letter_dropdown}")
-    
+    for vowel in vowels:
+        pronounce_arabic_letter_with_vowel(letter_dropdown, vowel)
