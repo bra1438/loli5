ca94ad53341c884499b2c0124768f938c24b9455
Brahim <32096282+bra1438@users.noreply.github.com>
2023-06-11 14:36:25 +0300

    Update app.py

--- app.py
+++ app.py
@@ -1,15 +1,17 @@
 import streamlit as st
 from gtts import gTTS
+import os
 
-def pronounce_arabic_letters(letters):
-    for letter in letters:
-        tts = gTTS(text=letter, lang='ar')
-        tts.save("output.mp3")
-        st.audio("output.mp3")
+vowels = ["ا", "و", "ي"]
+def pronounce_arabic_letter_with_vowel(letter, vowel):
+    for vowel in vowels:
+      tts = gTTS(text=letter + vowel, lang='ar')
+      tts.save(f"file_{vowel}.mp3" )
+      st.audio(f"file_{vowel}.mp3")
 
 st.title("تعليم نطق الخروف العربية ")
 
 letter = st.text_input("ادخل الحرف:")
 
 if letter:
-    pronounce_arabic_letters(letter)
+    pronounce_arabic_letter_with_vowel(letter, vowel)
