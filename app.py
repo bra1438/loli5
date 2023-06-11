b2e829b300d0f4b5b202e2f97ed96b23c11b63e8
Brahim <32096282+bra1438@users.noreply.github.com>
2023-06-11 14:45:23 +0300

    Update app.py

--- app.py
+++ app.py
@@ -20,6 +20,7 @@ def pronounce_arabic_letter_with_vowel(letter, vowel):
     for vowel in vowels:
         tts = gTTS(text=letter + vowel, lang='ar')
         tts.save(f"file_{vowel}.mp3" )
+        st.audio("output.mp3")
 
 st.title("تعليم نطق الخروف العربية ")
 
