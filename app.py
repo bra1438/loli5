56fc4718d0b444b36d72b7a62d72109a5ee6dd34
Brahim <32096282+bra1438@users.noreply.github.com>
2023-06-12 08:39:27 +0300

    Update app.py

--- app.py
+++ app.py
@@ -1,7 +1,16 @@
 import streamlit as st
 from gtts import gTTS
       
+word = st.text_input("اكتب الكلمة المراد تقسيمها الى احرف")
 
+tts = gTTS(text=word, lang='ar')
+    tts.save("word.mp3")
+    st.audio("word.mp3")
+    for char in word:
+        tts = gTTS(text=char, lang='ar')
+        tts.save(f"file_{char}.mp3")
+        st.audio(f"file_{char}.mp3")
+      
 vowels = ["ا", "و", "ي"]
 
 def pronounce_arabic_letter_with_vowel(letter, vowel):
@@ -23,13 +32,5 @@ if letter_dropdown:
     for vowel in vowels:
         pronounce_arabic_letter_with_vowel(letter_dropdown, vowel)
 
-word = st.text_input("اكتب الكلمة المراد تقسيمها اللا حروف")
 
-if word:
-    tts.save("word.mp3")
-    st.audio("word.mp3")
-    for char in word:
-        tts = gTTS(text=char, lang='ar')
-        tts.save(f"file_{char}.mp3")
-        st.audio(f"file_{char}.mp3")
 
