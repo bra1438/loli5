bcc101f5d50f76109f22c4d77bef2d48ad302bad
Brahim <32096282+bra1438@users.noreply.github.com>
2023-06-12 09:18:51 +0300

    Update app.py

--- app.py
+++ app.py
@@ -24,10 +24,10 @@ image = Image.open("ar.jpg")
 # Display the image
 st.image(image)
 
-    st.subheader('تقسيم حروف الكلمة')     
-    word = st.text_input("اكتب الكلمة المراد تقسيمها الى احرف")
+st.subheader('تقسيم حروف الكلمة')     
+word = st.text_input("اكتب الكلمة المراد تقسيمها الى احرف")
 
-    if word:
+if word:
           for char in word:
                   tts = gTTS(text=char, lang='ar')
                   tts.save(f"file_{char}.mp3")
@@ -38,25 +38,25 @@ st.image(image)
           tts.save("word.mp3")
           st.audio("word.mp3")
 
-    st.subheader("نطق الاحرف")
+st.subheader("نطق الاحرف")
 
-    vowels = ["ا", "و", "ي"]
+vowels = ["ا", "و", "ي"]
 
-    def pronounce_arabic_letter_with_vowel(letter, vowel):
+def pronounce_arabic_letter_with_vowel(letter, vowel):
         tts = gTTS(text=letter + vowel, lang='ar')
         tts.save(f"file_{vowel}.mp3")
         st.audio(f"file_{vowel}.mp3")
 
 
 
-    # Create a list of Arabic letters
-    arabic_letters = ["ا", "ب", "ت", "ث", "ج", "ح", "خ", "د", "ذ", "ر", "ز", "س", "ش", "ص", "ض", "ط", "ظ", "ع", "غ", "ف", "ق", "ك", "ل", "م", "ن", "ه", "و", "ي"]
+# Create a list of Arabic letters
+arabic_letters = ["ا", "ب", "ت", "ث", "ج", "ح", "خ", "د", "ذ", "ر", "ز", "س", "ش", "ص", "ض", "ط", "ظ", "ع", "غ", "ف", "ق", "ك", "ل", "م", "ن", "ه", "و", "ي"]
 
-    # Create a dropdown list with the Arabic letters
-    letter_dropdown = st.selectbox("اختر احد الحروف", arabic_letters)
+# Create a dropdown list with the Arabic letters
+letter_dropdown = st.selectbox("اختر احد الحروف", arabic_letters)
 
-    # If a letter is selected, pronounce it with all three vowels
-    if letter_dropdown:
+# If a letter is selected, pronounce it with all three vowels
+if letter_dropdown:
         st.write(f"لقد اخترت الحرف: {letter_dropdown}")
         for vowel in vowels:
             pronounce_arabic_letter_with_vowel(letter_dropdown, vowel)
